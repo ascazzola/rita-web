0a0d9e43a83a350232ae5e882ff2a78ca2c849a7
Adriano Scazzola <adriano.scazzola@gmail.com>
2021-01-13 20:07:33 -0300

    removed api/me user info will be obtained directly from angular app

--- backend/src/main/kotlin/edu/robocode/service/controllers/HomeController.kt
+++ backend/src/main/kotlin/edu/robocode/service/controllers/HomeController.kt
@@ -1,7 +1,5 @@
 package edu.robocode.service.controllers
 
-import org.keycloak.KeycloakPrincipal
-import org.keycloak.KeycloakSecurityContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,11 +12,6 @@ class HomeController {
     @GetMapping("version")
     fun getVersion() : Mono<VersionInfo> {
         return  Mono.just(VersionInfo("1.0.0")) //TODO create version management
-    }
-
-    @GetMapping("me")
-    fun getVersion(principal: KeycloakPrincipal<KeycloakSecurityContext>) : Mono<String> {
-        return  Mono.just(principal.name) //TODO create version management
     }
 }
 
