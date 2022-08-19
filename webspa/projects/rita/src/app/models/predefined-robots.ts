168c8ad9f4532375b850c6d14f01a322eaef3375
Adriano Scazzola <adriano.scazzola@gmail.com>
2022-08-19 11:19:00 -0300

    Added Predefined robots, added mambo to predefined robots + changed default to http instead of https

--- webspa/projects/rita/src/app/models/predefined-robots.ts
+++ webspa/projects/rita/src/app/models/predefined-robots.ts
@@ -1,10 +1,11 @@
 import { ListType } from "./list-type";
 
 export const PREDEFINED_ROBOTS : ListType[] = [
-  {id:'sample.Walls', name: 'Walls'},
   {id:'sample.Corners', name: 'Corners'},
+  {id:'sample.Mambo', name: 'Mambo'},
   {id:'sample.MyFirstJuniorRobot', name: 'MyFirstJuniorRobot'},
   {id:'sample.MyFirstRobot', name: 'MyFirstRobot'},
   {id:'sample.RamFire', name: 'RamFire'},
-  {id:'sample.TrackFire', name: 'TrackFire'}
+  {id:'sample.TrackFire', name: 'TrackFire'},
+  {id:'sample.Walls', name: 'Walls'},
 ];
